028dc3b5b1d3850abc90f3de7586a15ba7e8794e
diosamuel <koding12sam@gmail.com>
2025-11-20 19:49:02 +0700

    Initialize landing page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-slate-950 text-white font-display flex justify-center items-center h-screen">
+      <div className="text-center text-4xl font-bold justify-center items-center">
+        <h1 className="text-center text-4xl font-bold">UROTERA</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
